80c4ff5cbd14d341916bfb3bc9d0dadee7ca1184
Evan <rekreider@una.edu>
2018-10-02 14:07:19 -0500

    Basic Lemonade Stand

---
+++ LemonadeStand/IStand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LemonadeStand
+{
+    interface IStand
+    {
+        string Name { get; set; }
+        double PricePerItem { get; set; }
+        double CostPerItem { get; set; }
+        double TotalItemsSold { get; set; }
+
+        double TotalRevenue();
+        double TotalExpenses();
+        double TotalProfit();
+    }
+}
---
+++ LemonadeStand/LemonadeStand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LemonadeStand
+{
+    class LemonadeStand : IStand
+    {
+        public string Name { get; set; }
+        public int CupsSoldPerSeason { get; set; }
+        public double Chair { get; set; }
+        public double Sign { get; set; }
+        public double Table { get; set; }
+        public double Blender { get; set; }
+
+        public double PricePerCup { get; set; }
+        public double CostPerCup { get; set; }
+        public double PricePerItem { get; set; }
+        public double CostPerItem { get; set; }
+        public double TotalItemsSold { get; set; }
+
+        public double TotalRevenue()
+        {
+            return CupsSoldPerSeason * PricePerCup;
+        }
+
+        public double TotalExpenses()
+        {
+            return (CupsSoldPerSeason * CostPerCup) + Chair + Sign + Table + Blender;
+        }
+
+        public double TotalProfit()
+        {
+            return TotalRevenue() - TotalExpenses();
+        }
+    }
+}
