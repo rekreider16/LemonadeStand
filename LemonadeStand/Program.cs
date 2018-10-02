f39615fc92df06f753cf50a06fae84691ddc33c3
Evan <rekreider@una.edu>
2018-10-02 14:02:23 -0500

    Basic setup of Lemonade Stand

--- LemonadeStand/Program.cs
+++ LemonadeStand/Program.cs
@@ -1,5 +1,6 @@
-﻿
-using System;
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace LemonadeStand
 {
@@ -7,7 +8,26 @@ namespace LemonadeStand
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            LemonadeStand stand = new LemonadeStand();
+
+            Console.WriteLine("How much does a cup cost to make?");
+            string costResponse = Console.ReadLine();
+            stand.CostPerCup = double.Parse(costResponse);
+
+            Console.WriteLine("For how much will a cup be sold for?");
+            string priceResponse = Console.ReadLine();
+            stand.PricePerCup = double.Parse(priceResponse);
+
+            Console.WriteLine("How many cups have been sold this season?");
+            string cupsSoldResponse = Console.ReadLine();
+            stand.CupsSoldPerSeason = int.Parse(cupsSoldResponse);
+
+            Console.WriteLine($"Your revenue is: {stand.TotalRevenue()}");
+            Console.WriteLine($"Your expenses are: {stand.TotalExpenses()}");
+            Console.WriteLine($"Your current profit is: {stand.TotalProfit()}");
+            Console.ReadLine();
+
+
         }
     }
 }
